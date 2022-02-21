584541ec9698ecf85449293945a53e541650aaf4
jjesuscastro <jjoesei.castro@gmail.com>
2022-02-21 08:43:00 +0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,98 @@
+const { MongoClient, ObjectID } = require("mongodb");
+const Express = require("express");
+const BodyParser = require('body-parser');
+const e = require("express");
+
+const server = Express();
+
+server.use(BodyParser.json());
+server.use(BodyParser.urlencoded({ extended: true }));
+
+const client = new MongoClient("mongodb://localhost:27017");
+
+var patients;
+var records;
+
+//Create Patient
+server.post("/patients", async (request, response, next) => {
+    try {
+        let result = await patients.insertOne(request.body);
+        response.send(result);
+    } catch (e) {
+        response.status(500).send({ message: e.message });
+    }
+});
+
+//Create Record
+server.post("/records", async (request, response, next) => {
+    try {
+        let result = await records.insertOne(request.body);
+        response.send(result);
+    } catch (e) {
+        response.status(500).send({ message: e.message });
+    }
+});
+
+//Fetch patieents
+server.get("/patients", async (request, response, next) => {
+    try {
+        let result = await patients.find({}).toArray();
+        response.send(result);
+    } catch (e) {
+        response.status(500).send({ message: e.message });
+    }
+});
+
+//Fetch records
+server.get("/records", async (request, response, next) => {
+    try {
+        let result = await records.find({}).toArray();
+        response.send(result);
+    } catch (e) {
+        response.status(500).send({ message: e.message });
+    }
+});
+
+//Fetch specific patient
+server.get("/patients/:patientID", async (request, response, next) => {
+    try {
+        let result = await patients.findOne({ "patientID": request.params.patientID });
+        response.send(result);
+    } catch (e) {
+        response.status(500).send({ message: e.message });
+    }
+});
+
+//Update specific patient
+server.put("/patients/:patientID", async (request, response, next) => {
+    try {
+        let result = await patients.updateOne(
+            { "patientID": request.params.patientID },
+            { "$set": request.body }
+        );
+        response.send(result);
+    } catch (e) {
+        response.status(500).send({ message: e.message });
+    }
+});
+
+//Delete specific patient
+server.delete("/patients/:patientID", async (request, response, next) => {
+    try {
+        let result = await patients.deleteOne({ "patientID": request.params.patientID });
+        response.send(result);
+    } catch (e) {
+        response.status(500).send({ message: e.message });
+    }
+});
+
+server.listen("3000", async () => {
+    try {
+        await client.connect();
+        patients = client.db("GeeBee").collection("Patients");
+        records = client.db("GeeBee").collection("Records");
+        console.log("Listening at :3000...");
+    } catch (e) {
+        console.error(e);
+    }
+});
